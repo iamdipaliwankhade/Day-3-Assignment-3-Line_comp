2eb5dee44dc307e2ba564858de943330614597ac
iamdipaliwankhade <iamdipaliwankhade@gmail.com>
2022-09-20 23:06:27 +0530

    [Dipali] Added : compare two line

--- src/com/linecomparision/LineComparison.java
+++ src/com/linecomparision/LineComparison.java
@@ -1,4 +1,7 @@
 package com.linecomparision;
+
+import java.util.Scanner;
+
 /**
  * 
  * @author Dipali
@@ -10,17 +13,19 @@ public class LineComparison {
 		// TODO Auto-generated method stub
 
 		System.out.println("Welcome to Line Comparison Computation Program");
-
-		int x1 = 5;
-		int x2 = 4;
-		int y1 = 3;
-		int y2 = 8;
-		int x3 = 5;
-		int x4 = 4;
-		int y3 = 3;
-		int y4 = 8;
-		double lengthOfLine1 = Math.sqrt(((x2 - x1) ^ 2) + ((y2 - y1) ^ 2));
-		double lengthOfLine2 = Math.sqrt(((x4 - x3) ^ 2) + ((y4 - y3) ^ 2));
+		Scanner sc = new Scanner(System.in);
+		System.out.println("First Line Co ordinates");
+		int x1 = sc.nextInt();
+		int x2 = sc.nextInt();
+		int y1 = sc.nextInt();
+		int y2 = sc.nextInt();
+		System.out.println("Second Line Co ordinates");
+		int x3 = sc.nextInt();
+		int x4 = sc.nextInt();
+		int y3 = sc.nextInt();
+		int y4 = sc.nextInt();
+		double lengthOfLine1 = (int) Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
+		double lengthOfLine2 = (int) Math.sqrt(Math.pow((x4 - x3), 2) + Math.pow((y4 - y3), 2));
 		Integer obj1 = new Integer((int) lengthOfLine1);
 		Integer obj2 = new Integer((int) lengthOfLine2);
 		System.out.println("The points x1= " + x1);
@@ -34,5 +39,12 @@ public class LineComparison {
 		System.out.println("Length of Line1 = " + lengthOfLine1);
 		System.out.println("Length of Line2 =" + lengthOfLine2);
 		System.out.println("Line1 and line2 are Equal : " + obj1.equals(obj2));
+		System.out.println("Comparing 2 Lines : " + obj1.compareTo(obj2));
+		if (obj1.compareTo(obj2) > 0) {
+			System.out.println("Line One is big");
+		} else
+			System.out.println("Line two is big");
+
 	}
+
 }
