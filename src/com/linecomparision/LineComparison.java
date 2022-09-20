2c1c0b0001e3da5812487501b91a02f59c41f28d
iamdipaliwankhade <iamdipaliwankhade@gmail.com>
2022-09-20 19:19:08 +0530

    [Dipali] Added : Welcome Message

---
+++ src/com/linecomparision/LineComparison.java
@@ -0,0 +1,12 @@
+package com.linecomparision;
+
+public class LineComparison {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Welcome to Line Comparison Computation Program");
+
+	}
+
+}
